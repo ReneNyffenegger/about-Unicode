55676d2ae639e011d35944480a2a7f92750ee6ff
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-12-19 08:09:07 +0100

    More letters

--- Codepoints/sub-superscript.pl
+++ Codepoints/sub-superscript.pl
@@ -4,31 +4,60 @@ use strict;
 use utf8;
 
 
-my $subscript_letters = 'ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖʳˢᵗᵘᵛʷˣʸᶻ'; # Note: superscript q apparantly does not exist
+  my $superscript_letters   = 'ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖ#ʳˢᵗᵘᵛʷˣʸᶻ'; # Note: superscript q apparantly does not exist
+  my $superscript_LETTERS   = 'ᴬᴮ#ᴰᴱ#ᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾ#ᴿ#ᵀᵁⱽᵂ###';
+  my $superscript_SmallCaps = '########ᶦᶫ##ᶰ#######ᶸ#####';
+  my $subscript_letters     = 'ₐ###ₑ##ₕᵢⱼₖₗₘₙₒₚ#ᵣₛₜᵤᵥ#ₓ##';
+# my $overscript_letters    = '######◌ᷛ####◌ᷞ◌ᷟ◌ᷡ###◌ᷢ########';
+# my $underscript_letters   = '######◌ᷛ####◌ᷞ◌ᷟ◌ᷡ###◌ᷢ########';
 
 open my $html, '>:utf8', 'subscript-superscript.html';
 
 print $html qq{<html><head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Unicode: subscripts and superscripts</title>
+  <title>Unicode: subscript and superscript letters</title>
 </head>
 <body>
-<h1>Unicode: <strike>subscripts and </strike>superscripts</h1>
+<h1>Unicode: subscripts and superscripts letters</h1>
 };
 
   print $html "Note: superscript q is apparently missing!";
 
-  print $html "<table border=1><tr>";
+  print $html "<table border=1>";
 
+  printLetterRow($superscript_letters  );
+  printLetterRow($superscript_LETTERS  );
+  printLetterRow($superscript_SmallCaps);
+  printLetterRow($subscript_letters    );
+# printLetterRow($overscript_letters   );
 
-  for my $c (split //, $subscript_letters) {
-     print $html "<td><div style='font-size:60px'>$c</div>" . ord($c) . ' / ' . sprintf("%x", ord($c)) . "</td>";
-  }
   
-  print $html "</table></tr>";
+  print $html "</table>";
 
-print $html "See also <a href='Find-Unicode-Character-by-Appearance.html'>Find Unicode Character by visual Appearance</a>.";
+print $html "<p>See also <a href='Find-Unicode-Character-by-Appearance.html'>Find Unicode Character by visual Appearance</a>, <a href='https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts'>Wikipedia</a> etc.";
 
 print $html "</body></html>";
 
 close $html;
+
+sub printLetterRow {
+
+  my $letters = shift;
+
+  print $html "<tr>";
+
+  for my $c (split //, $letters) {
+     print $html "<td>";
+     
+     if ($c ne '#') {
+       print $html "<div style='font-size:60px'>$c</div>" . ord($c) . ' / ' . sprintf("%x", ord($c));
+     }
+     else {
+       print $html "n/a";
+     }
+     print $html "</td>";
+  }
+
+  print $html "</tr>";
+
+}
